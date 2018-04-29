4aca0c845d6583ab048b5c45b1179665b4059df3
Isaac Helbling <imhelbling@gmail.com>
2018-04-28 23:12:45 -0400

    refactored and renamed

---
+++ src/helpers/entity-groups.js
@@ -0,0 +1,50 @@
+const _ = require('lodash')
+const jp = require('fs-jetpack')
+const chalk = require('chalk')
+const allEntities = require('../constants/entity-groups/all.json')
+
+function loadGroup(groupName) {
+  const data = jp.read(`./src/constants/entity-groups/${groupName}.json`)
+
+  if (typeof data === 'undefined') {
+    const message = chalk.red(`Entity group '${groupName}' not found!`)
+    throw new Error(message)
+  }
+
+  return JSON.parse(data)
+}
+
+function formatGroup(group) {
+  return group.map(entity => {
+    return `type=!${entity}`
+  }).join(',')
+}
+
+function entityGroup(groupName) {
+  const group = loadGroup(groupName)
+
+  if (groupName !== 'all') {
+    const negative = _.pullAll(allEntities, group)
+    return formatGroup(negative)
+  } else {
+    return ''
+  }
+
+}
+
+function getAllGroups() {
+  const groupFiles = jp.list('./src/constants/entity-groups/')
+  let groups = {}
+  
+  groupFiles.forEach(file => {
+    const groupName = file.split('.')[0]
+    groups[groupName] = entityGroup(groupName)
+  })
+
+  return groups
+}
+
+// console.log((getAllGroups()));
+// console.log('@e[' + entityGroup('undead') + ']')
+
+module.exports = getAllGroups
