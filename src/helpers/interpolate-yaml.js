4697f115b1bd12d5a3105c9b6aa7d766cb5a8a07
Isaac Helbling <imhelbling@gmail.com>
2018-04-30 22:06:04 -0400

    yaml interpolator fails more gracefully when it finds an undefined variable

--- src/helpers/interpolate-yaml.js
+++ src/helpers/interpolate-yaml.js
@@ -12,17 +12,22 @@ function interpolateYaml(yamlStr, externalValues = {}, pass = 0) {
   const replaceables = yamlStr.match(/\$([a-z]|[A-Z]|\.)*/g)
 
   if (replaceables !== null) {
-    replaceables.map(instance => {
-      const yamlValue = _.at(rawJson, instance.replace('$', ''))
+    replaceables.forEach((instance, index) => {
+      const localValue = _.at(rawJson, instance.replace('$', ''))
       const externalValue = _.at(externalValues, instance.replace('$', ''))
       const sanitized = '\\'+instance
 
       const removeableQuotes = /"/g
 
-      if (typeof externalValue[0] !== 'undefined') {
+      if (typeof localValue[0] !== 'undefined') {
+        processedYaml = processedYaml.replace(new RegExp(sanitized, 'g'), JSON.stringify(localValue[0]).replace(removeableQuotes, ''))
+      } else if (typeof externalValue[0] !== 'undefined') {
         processedYaml = processedYaml.replace(new RegExp(sanitized, 'g'), JSON.stringify(externalValue[0]).replace(removeableQuotes, ''))
-      } else if (typeof yamlValue[0] !== 'undefined') {
-        processedYaml = processedYaml.replace(new RegExp(sanitized, 'g'), JSON.stringify(yamlValue[0]).replace(removeableQuotes, ''))
+      }
+      else {
+        // FIXME 'rawJson.name' will not work for other yaml content that doesnt have the 'name' field. Find a way to get the filename/path.
+        const message = `Value for '${instance}' not found while interpolating yaml file '${rawJson.name}'`
+        throw new Error(message)
       }
     })
   }
